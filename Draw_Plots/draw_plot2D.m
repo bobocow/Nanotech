0f22196b1c3927e0a7ce7c27cab1c2e57a5d2d00
bobocow <bobocow@users.noreply.github.com>
2024-02-02 16:05:32 +0800

    Update draw_plot2D.m
    Added code for drawing 2D plots with arrow vectors, a la Matlab help.

--- Draw_Plots/draw_plot2D.m
+++ Draw_Plots/draw_plot2D.m
@@ -15,3 +15,14 @@ xlabel('x')
 ylabel('sin(x) & cos(x)')
 
 legend('sin(x)','cos(x)')
+
+%A vector arrow plot
+x = -2:.2:2; 
+y = -1:.2:1;
+
+[xx,yy] = meshgrid(x,y);
+zz = xx.*exp(-xx.^2-yy.^2);
+[px,py] = gradient(zz,.2,.2);
+
+quiver(x,y,px,py)
+xlim([-2.5 2.5])    % set limits of x axis
