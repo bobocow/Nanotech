eef2cc14c1bd2303700a886fca7580d0e62718bd
bobocow <bobocow@users.noreply.github.com>
2024-02-02 10:51:07 +0800

    Add files via upload

---
+++ Functions_and_Siamese_Network/TrainASiameseNetworkToCompareImagesExample/getSimilarPair.m
@@ -0,0 +1,20 @@
+function [pairIdx1,pairIdx2,pairLabel] = getSimilarPair(classLabel)
+% getSimilarSiamesePair returns a random pair of indices for images
+% that are in the same class and the similar pair label = 1.
+
+    % Find all unique classes.
+    classes = unique(classLabel);
+    
+    % Choose a class randomly which will be used to get a similar pair.
+    classChoice = randi(numel(classes));
+    
+    % Find the indices of all the observations from the chosen class.
+    idxs = find(classLabel==classes(classChoice));
+    
+    % Randomly choose two different images from the chosen class.
+    pairIdxChoice = randperm(numel(idxs),2);
+    pairIdx1 = idxs(pairIdxChoice(1));
+    pairIdx2 = idxs(pairIdxChoice(2));
+    pairLabel = 1;
+end
+
