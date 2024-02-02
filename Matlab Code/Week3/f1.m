958f25c257a93472ccda1b21a166c7f2aafddf16
bobocow <bobocow@users.noreply.github.com>
2024-02-02 10:39:04 +0800

    Delete Matlab Code directory

--- Matlab Code/Week3/f1.m
+++
@@ -1,7 +0,0 @@
-function [f,g,h] = f1(x,c)
-if (nargin == 1), c = 1; end
-f = 1-x.*exp(-x.^2/c);
-if (nargout > 1), g = -exp(-x.^2) + 2*(x.^2).*exp(-x.^2);
-end
-if (nargout > 2), h = 6*x.*exp(-x.*x) - 4*x.^3.*exp(-x.*x);
-end
